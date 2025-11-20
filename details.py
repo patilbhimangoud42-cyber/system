4625c9368423b82d0e9a2f22a87d98f265638395
patilbhimangoud42-cyber <patilbhimangoud42@gmail.com>
2025-11-20 14:41:27 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,12 +1,12 @@
 import sys
 if len(sys.argv)  !=3:
-  print("usage: python details.py <name> <roll>")
+  print("usage: python details.py <name> <rollno>")
   sys.exit(1)
   script_name = sys.argv[0]
   name = sys.argv[1]
   rollno = sys.argv[2]
   print("script name:",script_name)
   print("student name:",name)
-  print("roll number :",rollon)
+  print("roll number :",rollno)
 
 
